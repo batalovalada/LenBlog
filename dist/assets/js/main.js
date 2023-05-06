c0f6fb462c52def9543d2aaf58e18310b8a930c6
lada <lada.batalova@gmail.com>
2023-05-06 16:54:04 +0300

    articlePages.js last v in all html

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -81,42 +81,35 @@ function closeModal(currentModal) {
 }
 //==============================ARTICLE PAGES=================================
 const acrticlePages = document.querySelectorAll('.articlePages');
-const pageCount = 3;
 
 acrticlePages.forEach(item => {
     const acrticlePage = item;
     const pagination = item.querySelectorAll('[data-page]');
+    const pageCount = pagination.length-2;
     pagination.forEach(item => {
         item.addEventListener('click', function(event) {
             event.preventDefault();
             const pageId = this.dataset.page;
             let page = acrticlePage.querySelector(pageId);
+
+            //number of page that is currently active
             let activeNowPageId;
             pagination.forEach(item => {
                 if (item.classList.contains('active')) activeNowPageId = item.dataset.page;
             });
+
+            //make all pages inactive
             pagination.forEach(item => {
                 item.classList.remove('active');
                 const hidePageId = item.dataset.page;
-                if (hidePageId !='#nextPage' && hidePageId !='#previousPage') acrticlePage.querySelector(hidePageId).classList.remove('show');
+                if (hidePageId !='.nextPage' && hidePageId !='.previousPage') acrticlePage.querySelector(hidePageId).classList.remove('show');
             });
+
+            //make necessary page active
             if (/\d+/gi.test(pageId)) {
                 this.classList.add('active');
-            } else if (pageId == '#nextPage') {
-                let num = activeNowPageId.match(/\d+/gi);
-                num = num[0];
-                if (num < pageCount) num++;
-                activeNowPageId = activeNowPageId.replace(/\d+/gi, `${num}`);
-                page = acrticlePage.querySelector(activeNowPageId);
-                pagination.forEach(item => {
-                    if (item.dataset.page == activeNowPageId) item.classList.add('active')
-                });
-
-            } else if (pageId == '#previousPage') {
-                let num = activeNowPageId.match(/\d+/gi);
-                num = num[0];
-                if (num > 1) num--;
-                activeNowPageId = activeNowPageId.replace(/\d+/gi, `${num}`);
+            } else if (pageId == '.nextPage' || pageId == '.previousPage') {
+                activeNowPageId = movePage(activeNowPageId, pageId, pageCount);
                 page = acrticlePage.querySelector(activeNowPageId);
                 pagination.forEach(item => {
                     if (item.dataset.page == activeNowPageId) item.classList.add('active')
@@ -126,6 +119,15 @@ acrticlePages.forEach(item => {
         });
     });
 });
+
+
+function movePage(activeNowPageId, pageId, pageCount) {
+    let num = activeNowPageId.match(/\d+/gi);
+    num = num[0];
+    if (pageId == '.nextPage' && num < pageCount) num++;
+    if (pageId == '.previousPage' && num > 1) num--;
+    return activeNowPageId.replace(/\d+/gi, `${num}`);
+}
 //=============================TEXTAREA=============================
 const textarea = document.querySelectorAll('[data-resize]');
 
